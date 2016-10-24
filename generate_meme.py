248332989ca29f847b22ac699cef42f3e678a8e5
ngarg <ngarg@calpoly.edu>
2016-10-24 11:23:32 -0700

    Added main folders.

---
+++ generate_meme.py
@@ -0,0 +1,7 @@
+
+def generator():
+   print 'Inside generate_meme.py'
+
+if __name__ == "__main__":
+   generator()
+
---
+++ tweet_loader.py
@@ -0,0 +1,7 @@
+
+def loader():
+   print 'Inside tweet_loader.py'
+
+if __name__ == "__main__":
+   loader()
+
