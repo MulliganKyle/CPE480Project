64e1d9aff9d610b49ef7b2ba0bf6d6a64483f576
Mason Stevenson <masonstevenson@pcp073996pcs.wireless.calpoly.edu>
2016-11-03 13:15:56 -0700

    updated tweet loader to perform a custom query. To use, run with the following command line args: python tweet_loader.py[#topic] [dumpFileName]

--- tweet_loader.py
+++ tweet_loader.py
@@ -13,17 +13,22 @@ united_states_woeid = 23424977
 non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 EXHAUST_TIME = 900 #15 mins
 DAILY_TIME = 86400 #24 hours
 NUM_OF_TWEETS = 1 #Per trending topic
-dumpFile = open('Twitter/Dumps/Dump.txt', 'a') #Append to dump file
+NUM_OF_TWEETS_CUSTOM = 100
+
 
 def rate_limit_status():
 	data = api.rate_limit_status()
 	print("Rate Limit Status:\n")
 	print("Trends: " + str(data['resources']['trends']['/trends/place']))
 	print("Searches: " + str(data['resources']['search']['/search/tweets']))
+
+def searches_left():
+	data = api.rate_limit_status()
+	return data['resources']['search']['/search/tweets']['remaining']
 
 def time_delay(timeToWait):
 	time.sleep(timeToWait)
@@ -37,13 +42,17 @@ def check_limit():
 		print("Exhausted limit...Waiting 15 mins.\n")
 		TimeDelay(WAIT_TIME)
 
-def dump_tweets(serialized_tweets):
-	pickle.dump(serialized_tweets, open( "Twitter/Dumps/SerializedTweets.p", "wb" ))
+def dump_tweets(serialized_tweets, dumpFileName):
+
+	dumpTextFile = open('Twitter/Dumps/' + dumpFileName + '.txt', 'a') #Append to dump file
+	dumpPickleFileName = 'Twitter/Dumps/' + dumpFileName + '.p'
+
+	pickle.dump(serialized_tweets, open(dumpPickleFileName, "wb" ))
 	for tweet in serialized_tweets:
-		dumpFile.write("--------------------------------------------------------------------------------------------------------------------------------------------\n")
-		dumpFile.write("Topic: " + tweet.topic + "\nTweet(s):\n")
-		dumpFile.write(tweet.text.encode('utf8') + "\n")
-		dumpFile.write("--------------------------------------------------------------------------------------------------------------------------------------------\n")
+		dumpTextFile.write("--------------------------------------------------------------------------------------------------------------------------------------------\n")
+		dumpTextFile.write("Topic: " + tweet.topic + "\nTweet(s):\n")
+		dumpTextFile.write(tweet.text.encode('utf8') + "\n")
+		dumpTextFile.write("--------------------------------------------------------------------------------------------------------------------------------------------\n")
 
 def scrub_tweet(tweet):
 	# Take out RT's
@@ -52,15 +61,19 @@ def scrub_tweet(tweet):
 	# Take out hyper links
 	tweet.text = re.sub(r"http\S+", "", tweet.text)
 
-#   Pulls tweets from twitter
-#   Scrubs each one
-#   Then places them in a set of Tweet objects
-#   Returns set to be written to file.
-def pull_tweets():
+#Accesses the top trending topics from twitter and pulls NUM_OF_TWEETS tweets
+def top_tweets_dump():
 	tweets_to_analyze = set()
+	
+	#trends_place returns the top 50 trending topics, if available
 	trends = api.trends_place(united_states_woeid)[0]['trends']
 	trendNames = [trend['name'] for trend in trends]
+	print(trendNames)
+	return tweets_to_analyze
+
 	for trendName in trendNames:
+
+		#get NUM_OF_TWEETS tweets per trendName (max 100)
 		tweets = tweepy.Cursor(api.search, q = trendName).items(NUM_OF_TWEETS)
 		for tweet in tweets:
 			new_tweet_object = Tweet(trendName, tweet.text.translate(non_bmp_map))
@@ -70,9 +83,51 @@ def pull_tweets():
 			tweets_to_analyze.add(new_tweet_object)
 	return tweets_to_analyze
 
+def custom_dump(desiredTopic): 
+	print("inside custom_dump")
+	tweets_to_analyze = set()
+	searches = searches_left()
+	count = 0
+
+	while (searches > 1):
+
+		#get NUM_OF_TWEETS tweets per trendName (max 100)
+		tweets = tweepy.Cursor(api.search, q = desiredTopic).items(NUM_OF_TWEETS_CUSTOM)
+
+		for tweet in tweets:
+			count += 1
+			new_tweet_object = Tweet(desiredTopic, tweet.text.translate(non_bmp_map))
+			#print("Pre-scrub:\n" + new_tweet_object.text + "\n")
+			scrub_tweet(new_tweet_object)
+			#print("Post-scrub:\n" + new_tweet_object.text + "\n")
+			tweets_to_analyze.add(new_tweet_object)
+
+		print("got " + str(count) + " tweets")
+		count = 0
+
+		searches = searches_left()
+		print("searches remaining: " + str(searches))
+
+	return tweets_to_analyze
+
 # Main entry point for the program
 def loader():
 	print('Inside tweet_loader.py')
+	isCustomDump = False
+	desiredTopic = ''
+	dumpFileName = ''
+
+	if len(sys.argv) == 3 :
+		desiredTopic = sys.argv[1]
+		dumpFileName = sys.argv[2]
+		print(isCustomDump)
+		isCustomDump = True
+		print(isCustomDump)
+
+		print("Performing custom twitter dump for topic: " + desiredTopic)
+
+	else :
+		print("Performing top-tweets dump")
 
 	#print rate limit status
 	rate_limit_status()
@@ -81,10 +136,17 @@ def loader():
 	check_limit()
 
 	#Pull tweets
-	serialized_tweets = pull_tweets()
+	if(isCustomDump == True):
+		serialized_tweets = custom_dump(desiredTopic)
 
-	#Dump tweets
-	dump_tweets(serialized_tweets)
+		#Dump tweets
+		dump_tweets(serialized_tweets, dumpFileName)
+
+	else:
+		serialized_tweets = top_tweets_dump()
+
+		#Dump tweets
+		dump_tweets(serialized_tweets, "SerializedTweets")
 
 	#Rate limit status
 	rate_limit_status()
