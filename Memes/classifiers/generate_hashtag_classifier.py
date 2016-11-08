5a11c0d21a780d3423f00f8dacd56f9dcdba845b
Work Ehm <ehm@hm.edu>
2016-11-07 16:00:53 -0800

    hashtag classifier finalized. only needs better emoji removal.

--- Memes/classifiers/generate_hashtag_classifier.py
+++ Memes/classifiers/generate_hashtag_classifier.py
@@ -12,29 +12,40 @@ hashtag/emotion w/e
 
 import re
 import pickle
+import string
 import nltk
+from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.util import ngrams
 from Twitter.Tweet import *
 from Memes.classifiers.helpers import *
 
-PATH = "Twitter/Dumps/SerializedTweets.p"
+PATH = "Twitter/Dumps/"
 
 def generate_features():
    return
 
 
 def clean_tweets(tweets):
-# Remove hashtags, @'s, emojis
+   # Remove hashtags, @'s, emojis
+   # TODO: Better emoji removal
+   emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           "]+", flags=re.UNICODE)
    for tweet in tweets:
-       print("Pre-scrub: " + tweet.text)
-       tweet.text = ' '.join(word for word in tweet.text.split(' ')
-                                  if not word.startswith('#') and not word.startswith('@'))
-       print("Post-scrub: " + tweet.text)
+      tweet.text = ' '.join(word for word in tweet.text.split(' ')
+                                 if not word.startswith('#') and not word.startswith('@'))
+      tweet.text = re.sub(emoji_pattern, '', tweet.text)
 
 #extrat single words as features from the training data
 def generate_unigram_feature_list(training_data):
-   return set(word.lower() for passage in training_data for word in word_tokenize(passage))
+   unigram_features = set(word.lower() for passage in training_data for word in word_tokenize(passage))
+   unigram_features = set(word for word in unigram_features if word not in stopwords.words("english"))
+   unigram_features = set(word for word in unigram_features if word not in string.punctuation)
+   return unigram_features
 
 #extract bigrams as features form the training data
 def generate_bigram_feature_list(training_data):
@@ -43,6 +54,12 @@ def generate_bigram_feature_list(training_data):
       for gram in ngrams(word_tokenize(sentence), 2):
          if gram not in features_bigrams:
             features_bigrams.add(gram)
+   features_bigrams = set(bigram for bigram in features_bigrams 
+                                 if bigram[0] not in stopwords.words("english") 
+                                 and bigram[1] not in stopwords.words("english"))
+   features_bigrams = set(bigram for bigram in features_bigrams 
+                                 if bigram[0] not in string.punctuation 
+                                 and bigram[1] not in string.punctuation)
    return features_bigrams
 
 
@@ -60,22 +77,26 @@ def generate_features(tweet, word_features, features_bigrams):
    return bigram_training_features
 
 def main():
-# Prompt user for what to train on...
 
-# Ex: confused
-# Unpickle tweets
-   dump = open(PATH, "rb")
+   # Unpickle tweets
+   dump_waitwhat = open(PATH + "waitwhat_dump.p" , "rb")
+   dump_justdoit = open(PATH + "justdoit_dump.p" , "rb")
+   dump_unambiguous = open(PATH + "unambiguous_dump.p" , "rb")
 
-   # Load multiple dumps
-   # #waitwhat, #unambiguous, #justdoit
-   tweets = pickle.load(dump)
+   tweets_waitwhat = pickle.load(dump_waitwhat)
+   tweets_justdoit = pickle.load(dump_justdoit)
+   tweets_unambiguous = pickle.load(dump_unambiguous)
 
    # Scrub tweets
-   clean_tweets(tweets)
+   clean_tweets(tweets_waitwhat)
+   clean_tweets(tweets_justdoit)
+   clean_tweets(tweets_unambiguous)
 
-# TODO: create lists with confused not confused tweets
-   confused_tweets = []
-   not_confused_tweets = []
+
+
+   confused_tweets = set(tweet.text for tweet in tweets_waitwhat)
+   not_confused_tweets = set(tweet.text for tweet in tweets_justdoit) | set(tweet.text for tweet in tweets_unambiguous)
+
 
    confused_tweets = [x.lower() for x in confused_tweets]
    not_confused_tweets = [x.lower() for x in not_confused_tweets]
@@ -84,8 +105,6 @@ def main():
    # Extract the features from the training data
    word_features = generate_unigram_feature_list(all_tweets)
    bigram_features = generate_bigram_feature_list(all_tweets)
-# TODO:  This uses all "words" in the tweets as features.
-#        Filter out punctuation and and specific word types as articles.
 
    # Generate features.
    data = []
